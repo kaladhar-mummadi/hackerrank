b837354ec46f1a815e6e9cf25a1b33b232eba90f
kaladhar-mummadi <kaladhar.mummadi@gmail.com>
2016-10-21 00:47:03 +0530

    PE:pythogoream triplet

--- Project Euler/#9: Special Pythogorean Triplet.py
+++ Project Euler/#9: Special Pythogorean Triplet.py
@@ -1,57 +1,30 @@
 def _hack_getTripet(n):
-    if n > 11 and n%2 != 0 :
-        print(-1)
-        return
-    val = n // 2
-    i = 1
-    x = 1
-    while i < n:
-        temp = (val // i) - i
-        if temp == 0:
-            break
-        x = i
-        y = temp
-        i += 1
-    a = x * x - y * y
-    b = 2 * x * y
-    c = x * x + y * y
-    result = a * b * c
-    if result > 0:
-        print(result)
-    else:
-        print(-1)
+    a = 2
+    high = n//2
+    b = 0
+    c = 0
+    result = 0
+    while a < high:
+        numerator = ((a-n)*(a-n)) - (a*a)
+        denominator = 2*(n-a)
+        b = numerator // denominator
+        c = n - b - a
 
-def _euler_getTriplet():
-    n = 1000
-    sum = 0
-    while sum < 1000:
-        print(sum)
-        if n > 11 and n%2 != 0 :
-            print(-1)
-            return
-        val = n // 2
-        i = 1
-        x = 1
-        while i < n:
-            temp = (val // i) - i
-            if temp == 0:
-                break
-            x = i
-            y = temp
-            i += 1
-        a = x * x - y * y
-        b = 2 * x * y
-        c = x * x + y * y
-        result = a * b * c
-        sum = a + b + c
-    if n == sum:
-        print(result)
+        sq = a*a + b*b
+
+
+        if sq == c*c and b > 0 and c > 0:
+            temp = a*b*c
+            if result < temp:
+                result = temp
+        a += 1
+    if result == 0:
+        print("-1")
     else:
-        print(-1)
+        print(result)
 
 if __name__ == '__main__':
     T = int(input())
-    for _ in range(T):
+    for _ in range(T+1):
         N = int(input())
         _hack_getTripet(N)
-        #_euler_getTriplet()
